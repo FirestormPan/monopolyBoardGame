a37dc05aa1282829bc7155dde4d76345567b32a7
unalrt <unalamet3@gmail.com>
2024-04-17 21:12:36 +0300

    added functions payRentTo, changeBalanceBy, jailStatus, playerStatus

--- src/main/java/dai/monopoly/Player.java
+++ src/main/java/dai/monopoly/Player.java
@@ -3,37 +3,47 @@ package dai.monopoly;
 import java.util.ArrayList;
 import java.util.List;
 
-import dai.monopoly.board.Board;
 import dai.monopoly.board.properties.Property;
-import lombok.Getter;
-
 
 public class Player {
-	public Player(String colour) {
-		super();
-		this.colour = colour;
-		this.balance = 5000; // h osa einai pou 3ekiname anyway
-		this.ownerships = new ArrayList();
-		this.isPrisoned = false;
-	}
-
-	@Getter
-	private String colour;
 	private int balance;
-	private List<Property> ownerships; 
-	private boolean isPrisoned; //?
+	private String colour;
+	private int position;
+	private List<Property> ownerships;
+	private boolean isInGame;
+	private boolean isPrisoned;
+
+	public Player(int id, int position) {
+		this.colour = colour;
+		this.balance = 400; 
+		this.position = position;
+		this.ownerships = new ArrayList();
+		this.isPrisoned = jailStatus(this.position);
+		this.isInGame = playerStatus(this.balance);
+		
+	}
 	
+	public boolean jailStatus(int position ) {
+		return (position == 10); // 10 is not permanent
+			
+	}
+	
+	public boolean playerStatus(int balance ) {
+		return (balance > 0); 
+			
+	}
 	
 	public int changeBalanceBy(int payment) {
-		throw new UnsupportedOperationException("Feature under development");
-	}
-
-	public int payRentTo(Player landowner) {
-		throw new UnsupportedOperationException("Feature under development");
-	}
-
-	public String getColour() {
-		return this.colour;
+		balance = balance - payment;
+		playerStatus(balance);
+		return balance;
 	}
 	
+	public void payRentTo(Player landowner,int rent) {
+		landowner.balance = landowner.balance + rent;
+		changeBalanceBy(rent);
+		
+	}
+	
+
 }
