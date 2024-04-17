586d36c34c651ed9f94907ada25cd3aac1aa6e95
Pantelis <pantelos1999@gmail.com>
2024-04-17 20:18:29 +0300

    added rollDice function

--- src/main/java/dai/monopoly/GameManager.java
+++ src/main/java/dai/monopoly/GameManager.java
@@ -1,11 +1,19 @@
 package dai.monopoly;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class GameManager {
 
 	Queue<Player> activePlayers = new LinkedList<>();
+	
+	public GameManager(){
+		activePlayers.add(new Player("green"));
+		activePlayers.add(new Player("red"));
+		activePlayers.add(new Player("yellow"));
+	}
+		
 	
 	public void getTheGameRunningBre() {
 		boolean conditionForEnd = false; 
@@ -16,14 +24,26 @@ public class GameManager {
 			maketurnMovesFor(turnPlayer);			
 			//if player didnt lose, put player back to queue
 			activePlayers.add(turnPlayer);
+				
+			
+			conditionForEnd = activePlayers.size()>1;
 		}
 	}
 	
 	public void maketurnMovesFor(Player player) { //(Here or on Board Class?)
 		//roll dice --> get out of jail, if jailed dont move etc
+		rollDice();
 		//move (Board)
 		//do  landing actions
 		// if it was a double roll, play again
 	}
+	
+	public int[] rollDice() {
+		return new int [] {
+			(int) Math.ceil(Math.random() * 6), //Math.random doesn't return 0 ever, so we good
+			(int) Math.ceil(Math.random() * 6)
+		};
+	}
+	
 }
  
